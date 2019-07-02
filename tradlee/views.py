2dfd197e7fbf08e6716e9886147ac89e94c8a3c4
janglee123 <merupatel123@gmail.com>
2019-07-02 14:26:33 +0530

    routes added

---
+++ tradlee/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+
+def home(request):
+
+	return HttpResponse('Home')
+	pass
+
+def ad_post(request):
+
+	return HttpResponse('ad_post')
+	pass
+
+def account(request, username):
+
+	return HttpResponse('account ' + username)
+	pass

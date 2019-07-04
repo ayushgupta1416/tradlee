f7b97d7ef5508acbb37effa5dcba81a5b6e24601
janglee123 <merupatel123@gmail.com>
2019-07-04 16:18:24 +0530

    login form

--- tradlee/views.py
+++ tradlee/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from .forms import sellForm, rentForm, rentFeedbackForm, sellFeedbackForm
+from django.contrib.auth import login, authenticate
+# from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from .forms import SellForm, RentForm, RentFeedbackForm, SellFeedbackForm, SignupForm
 from .models import *
-
 
 def home(request):
     return HttpResponse('Home')
@@ -11,12 +13,12 @@ def home(request):
 
 def sell(request):
     if request.method == 'POST':
-        form = sellForm(request.POST, request.FILES)
+        form = SellForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/')
     else:
-        form = sellForm()
+        form = SellForm()
     context = dict()
     context['form'] = form
     temp = loader.get_template('tradlee/sell.html')
@@ -26,12 +28,12 @@ def sell(request):
 
 def rent(request):
     if request.method == 'POST':
-        form = rentForm(request.POST, request.FILES)
+        form = RentForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/')
     else:
-        form = rentForm()
+        form = RentForm()
     context = dict()
     context['form'] = form
     temp = loader.get_template('tradlee/rent.html')
@@ -42,7 +44,7 @@ def rentfeedback(request, p_id):
     if request.method == 'POST':
         return HttpResponse('feedback submitted for product id' + p_id)
     context = dict()
-    context['form'] = rentFeedbackForm()
+    context['form'] = RentFeedbackForm()
     temp = loader.get_template('tradlee/rentFeed.html')
     return HttpResponse(temp.render(context, request))
 
@@ -51,11 +53,25 @@ def sellfeedback(request, p_id):
     if request.method == 'POST':
         return HttpResponse('feedback submitted for product id' + p_id)
     context = dict()
-    context['form'] = sellFeedbackForm()
+    context['form'] = SellFeedbackForm()
     temp = loader.get_template('tradlee/sellFeed.html')
     return HttpResponse(temp.render(context, request))
 
 
-def account(request, username):
+def user(request, username):
     return HttpResponse('account ' + username)
     pass
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
